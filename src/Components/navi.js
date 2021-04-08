434d5cea920f54b20276a2285337b951a0431c7a
Jaclyn <jaclnmarcum@gmail.com>
2021-04-08 10:49:16 -0400

    added some styling, dropdownButton, Navigation, Header, installed bootstrap and sass

--- src/Components/navi.js
+++ src/Components/navi.js
@@ -1,18 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Button, Container, Dropdown, ButtonGroup } from "react-bootstrap";
 
 function Navi(props) {
   return (
-    <div>
-      <nav>
-        <button>
-          <Link to="/Hompage">Home</Link>
-        </button>
-        <button>
-          <Link to="/products">All Products</Link>
-        </button>
-      </nav>
-    </div>
+    <Container className="navi">
+      <div>
+        <Link to="/" className="btn btn-green">
+          Home
+        </Link>
+
+        <Dropdown as={ButtonGroup}>
+          <Button variant="success">Products</Button>
+
+          <Dropdown.Toggle split variant="success" id="dropdown-basic" />
+          <Dropdown.Menu>
+            <Dropdown.Item>
+              <Link to="/Computers">Computers</Link>
+            </Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item>
+              <Link to="/Headphones">Headphones</Link>
+            </Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item>
+              <Link to="/Televisions">Televisions</Link>
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+    </Container>
   );
 }
 
---
+++ src/Views/Computers.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Computers(props) {
+  return <div>List of Computers</div>;
+  //return .filter of product in Computer deparment
+}
+
+export default Computers;
