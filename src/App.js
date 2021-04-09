76c4b3702debb0a08d832a473b5f7cad5796d597
Jaclyn <jaclnmarcum@gmail.com>
2021-04-09 10:43:42 -0400

    added sign up

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import "./App.scss";
 import { Route, Switch } from "react-router-dom";
 import Homepage from "./views/Homepage";
-import Navi from "./Components/Navi";
+import Navi from "./components/Navi";
 import Computers from "./views/Computers";
 import Headphones from "./views/Headphones";
 import Televisions from "./views/Televisions";
+import SignUp from "./views/SignUp";
+import NotFound from "./views/NotFound";
+import { Container } from "react-bootstrap";
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Electronic Store</h1>
-      <Navi />
+      <h1>
+        Welcome to Electronic Store
+        <Navi />
+      </h1>
+
       <Switch>
         <Route exact path="/" component={Homepage} />
+        <Route path="/SignUp" component={SignUp} />
         <Route path="/Computers" component={Computers} />
         <Route path="/Headphones" component={Headphones} />
         <Route path="/Televisions" component={Televisions} />
+        <Route component={NotFound} />
       </Switch>
     </div>
   );
--- src/Components/navi.js
+++ src/Components/navi.js
@@ -5,30 +5,28 @@ import { Button, Container, Dropdown, ButtonGroup } from "react-bootstrap";
 function Navi(props) {
   return (
     <Container className="navi">
-      <div>
-        <Link to="/" className="btn btn-green">
-          Home
-        </Link>
+      <Link to="/" className="btn btn-green">
+        Home
+      </Link>
 
-        <Dropdown as={ButtonGroup}>
-          <Button variant="success">Products</Button>
+      <Dropdown as={ButtonGroup}>
+        <Button variant="success">Products</Button>
 
-          <Dropdown.Toggle split variant="success" id="dropdown-basic" />
-          <Dropdown.Menu>
-            <Dropdown.Item>
-              <Link to="/Computers">Computers</Link>
-            </Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item>
-              <Link to="/Headphones">Headphones</Link>
-            </Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item>
-              <Link to="/Televisions">Televisions</Link>
-            </Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </div>
+        <Dropdown.Toggle split variant="success" id="dropdown-basic" />
+        <Dropdown.Menu>
+          <Dropdown.Item>
+            <Link to="/Computers">Computers</Link>
+          </Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item>
+            <Link to="/Headphones">Headphones</Link>
+          </Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item>
+            <Link to="/Televisions">Televisions</Link>
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
     </Container>
   );
 }
