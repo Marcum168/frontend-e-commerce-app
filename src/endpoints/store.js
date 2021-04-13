5080c5c2c2cd6ca687fb44b0e7ec85a1134706d4
Rachel Gulliver <rachelgulliver971@gmail.com>
2021-04-13 11:08:33 -0400

    now with endpoints

---
+++ src/endpoints/store.js
@@ -0,0 +1,35 @@
+import create from "zustand";
+import { devtools, redux } from "zustand/middleware";
+// import {useReducer} from 'react'
+
+const initialState = {products: {},  cart: '', balance: 2000, spent: 0 };
+
+export const ADDTOCART = 'ADDTOCART';
+export const PURCHASE = "PURCHASE";
+export const SEARCHITEM = 'SEARCHITEM';
+export const FILTERITEM = "FILTERITEM"
+export const DISPLAYPRODUCTS = 'DISPLAYPRODUCTS'
+export const DELETEFROMCART = 'DELETEFROMCART'
+export const MANAGEBALANCE = 'MANAGEBALANCE'
+
+const reducer = (state, action) => {
+switch(action.type){
+    case ADDTOCART:
+        return {cart: action.payload};
+    case PURCHASE:
+        return {};
+    case SEARCHITEM:
+        return {products: action.payload};
+    case FILTERITEM:
+        return {products: action.payload};
+    case DISPLAYPRODUCTS:
+        return {products: action.payload};
+    case DELETEFROMCART:
+        return {cart: action.payload};
+    case MANAGEBALANCE:
+        return {balance: action.payload};
+    default:
+        return state;
+}
+}
+export const useStore = create(devtools(redux(reducer, initialState)))
