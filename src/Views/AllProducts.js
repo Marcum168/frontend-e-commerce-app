3b49cf6861a417882555249204602e41a74ebdc2
Jaclyn <jaclnmarcum@gmail.com>
2021-04-20 12:30:50 -0400

    fixing merge conflict

---
+++ src/Views/AllProducts.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+function AllProducts(props) {
+  return (
+    <Container className="allProductsView">
+      <div>List of All Products</div>
+    </Container>
+  );
+  //return .filter of product in Computer deparment
+}
+
+export default AllProducts;
