6ed03047057f43494d66de3ad643dccbc24fe452
Jaclyn <jaclnmarcum@gmail.com>
2021-04-21 13:55:51 -0400

    working on merge conflict

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Headphones from "./views/Headphones";
 import Televisions from "./views/Televisions";
 import SignUp from "./views/SignUp";
 import NotFound from "./views/NotFound";
-import { Container } from "react-bootstrap";
+
 function App() {
   return (
     <div className="App">
@@ -17,11 +17,21 @@ function App() {
       </h1>
 
       <Switch>
-        <Route exact path="/" component={Homepage}><Homepage/></Route> 
-        <Route path="/SignUp" component={SignUp}><SignUp/></Route>
-        <Route path="/Computers" component={Computers} ><Computers/></Route>
-        <Route path="/Headphones" component={Headphones} ><Headphones/></Route>
-        <Route path="/Televisions" component={Televisions}><Televisions/></Route> 
+        <Route exact path="/" component={Homepage}>
+          <Homepage />
+        </Route>
+        <Route path="/SignUp" component={SignUp}>
+          <SignUp />
+        </Route>
+        <Route path="/Computers" component={Computers}>
+          <Computers />
+        </Route>
+        <Route path="/Headphones" component={Headphones}>
+          <Headphones />
+        </Route>
+        <Route path="/Televisions" component={Televisions}>
+          <Televisions />
+        </Route>
         <Route component={NotFound} />
       </Switch>
     </div>
